92db0a56ddf60fb5396f538649b3204a00090ec1
dilsonbharath <dilsonbharath76@gmail.com>
2026-02-18 17:26:42 +0530

    deploy ready

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,12 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
-
+import os
 
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = "sqlite:///./habits.db"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./habits.db")
     
     class Config:
         env_file = ".env"
